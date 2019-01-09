893fa15483c9f1720d15c31556d6eaeb914674e4
lloydie <lloydie.org@gmail.com>
2019-01-09 14:27:36 +1100

    ci fixes

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 def func(x):
     return x + 1
 
+def test_docker():
+    docker image ls
+    return 0
+
 def test_answer():
     assert func(3) == 4
+
+def test_docker():
+    assert docker_test() == 1
